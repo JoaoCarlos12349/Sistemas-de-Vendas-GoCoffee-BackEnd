44fb15d7dbf14371a127b27aff9335f4d660ea3e
João Carlos Martins <joaocarlos12349@gmail.com>
2025-06-06 10:00:21 -0300

    1.0 Dependências

---
+++ src/main/java/SVGoCoffee/SVGoCoffee/SistemaDeVendasGoCoffeeApplication.java
@@ -0,0 +1,13 @@
+package SVGoCoffee.SVGoCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVendasGoCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVendasGoCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SVGoCoffee/SVGoCoffee/SistemaDeVendasGoCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package SVGoCoffee.SVGoCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVendasGoCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
